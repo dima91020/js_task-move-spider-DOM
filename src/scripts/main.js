173ef1339878de83faa5e9fab0927402564a79c2
dima91020 <vereskodima76@gmail.com>
2025-07-08 15:16:04 +0300

    add main.js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,4 +2,32 @@
 
 document.addEventListener('click', (e) => {
   // write code here
+  const spider = document.querySelector('.spider');
+  const wall = document.querySelector('.wall');
+  const wallRect = wall.getBoundingClientRect();
+  const border = parseFloat(getComputedStyle(wall).borderLeftWidth);
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const closestWall = e.target.closest('.wall');
+
+  if (!closestWall) {
+    return;
+  }
+
+  let newPositionX = mouseX - wallRect.x - spider.offsetWidth / 2 - border;
+  let newPositionY = mouseY - wallRect.y - spider.offsetHeight / 2 - border;
+
+  newPositionX = Math.max(
+    0,
+    Math.min(newPositionX, wall.clientWidth - spider.offsetWidth),
+  );
+
+  newPositionY = Math.max(
+    0,
+    Math.min(newPositionY, wall.clientHeight - spider.offsetHeight),
+  );
+
+  spider.style.left = `${newPositionX}px`;
+  spider.style.top = `${newPositionY}px`;
 });
